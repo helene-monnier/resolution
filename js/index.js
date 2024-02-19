3a136cd6c8aaa985662bde2d8b01373681c8ffe0
helene-monnier <monnier.helene@outlook.com>
2024-02-19 14:32:42 +0100

    ajout de la resolution random + transitions

--- js/index.js
+++ js/index.js
@@ -1,11 +1,80 @@
 // import resolutions from ('../data/resolutions');
+const resolutions = [
+  {
+      id: 1,
+      texte: 'Collectionner les chaussettes orphelines et organiser une cérémonie annuelle pour leur rendre hommage.'
+  },
+  {
+      id: 2,
+      texte: 'Devenir un expert en jonglage avec des objets de la vie quotidienne.'
+  },
+  {
+      id: 3,
+      texte: 'Savoir jouer la Valse d’Amélie au piano.'
+  },
+  {
+      id: 4,
+      texte: 'Organiser une compétition de chasse aux trésors dans ma ville, avec des énigmes cachées dans les endroits les plus insolites.'
+  },
+  {
+      id: 5,
+      texte: 'Apprendre quelque chose de nouveau chaque mois.'
+  },
+  {
+      id: 6,
+      texte: 'Consacrer du temps à la méditation ou à la pleine conscience'
+  },
+  {
+      id: 7,
+      texte: 'Lire au moins un livre par mois pour nourrir mon esprit.'
+  },
+  {
+      id: 8,
+      texte: 'Réduire ma consommation de viande.'
+  },
+  {
+      id: 9,
+      texte: 'Organiser une soirée cinéma en regardant uniquement des films des années 80 en mangeant des snacks cosmiques.'
+  },
+  {
+      id: 10,
+      texte: 'Avoir un setup incroyable.'
+  },
+  {
+      id: 11,
+      texte: 'Développer une application mobile pour traduire les miaulements de mon chat en messages codés.'
+  },
+  {
+      id: 12,
+      texte: 'Faire un projet d’apothéose qui déchire.'
+  },
+  {
+      id: 13,
+      texte: 'Créer un portfolio pour présenter mes compétences'
+  },
+  {
+      id: 14,
+      texte: 'Participer à des hackathons pour résoudre des problèmes technologiques intéressants.'
+  },
+  {
+      id: 15,
+      texte: 'Suivre des cours en ligne pour me tenir au courant des dernières tendances et technologies.'
+  },
+  {
+      id: 16,
+      texte: 'Obtenir mon titre pro'
+  },
+  {
+      id: 17,
+      texte: 'Trouver une entreprise sympa pour faire CDA en contrat de pro'
+  },
+];
 
 const app = {
   init: () => {
     app.container = document.querySelector("#app");
 
     app.containerResolution();
-    app.resolution();
     app.containerActive();
     app.h1Elm();
     app.buttonElm();
@@ -18,10 +87,13 @@ const app = {
   },
 
   resolution: () => {
-    app.resolution = document.createElement('p');
-    app.resolution.textContent = 'Développer une application mobile pour traduire les miaulements de mon chat en messages codés. ';
+    const randomIndex = Math.floor(Math.random() * resolutions.length);
+    const randomResolution = resolutions[randomIndex];
+    app.resolutionElm = document.createElement('p');
+    app.resolutionElm.textContent = randomResolution.texte;
+    app.resolutionElm.classList.add('resolution-item'); 
     parentElm = document.querySelector('#containerResolution');
-    parentElm.append(app.resolution);
+    parentElm.append(app.resolutionElm);
   },
 
   containerActive: () => {
@@ -32,7 +104,7 @@ const app = {
 
   h1Elm: () => {
     app.h1Elm = document.createElement('h1');
-    app.h1Elm.textContent = 'Chiche de tenir cette résolution ?';
+    app.h1Elm.textContent = 'Découvre ta nouvelle résolution';
     parentElm = document.querySelector('#containerActive');
     parentElm.append(app.h1Elm);
   },
@@ -43,6 +115,14 @@ const app = {
     app.buttonElm.textContent = 'Résolution';
     parentElm = document.querySelector('#containerActive');
     parentElm.append(app.buttonElm);
+
+    // écouteur d'évent au click sur le button
+    app.buttonElm.addEventListener('click', () => {
+      if (app.resolutionElm) {
+        app.resolutionElm.remove();
+      }
+      app.resolution();
+    })
   },
 
 }
